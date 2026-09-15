594fb6685e766133c3c8270159224c4834c8b3ed
Ian-Alexander-Cruz <icruzm2405@alumno.ipn.mx>
2026-09-14 18:58:46 -0600

    js completo

--- primer parcial/01 Validaciones/js/validaciones.js
+++ primer parcial/01 Validaciones/js/validaciones.js
@@ -9,3 +9,38 @@ Regulares, las cuales los vamos a dividir en 3:
 Las expresiones regulares, son patrones que nos 
 ayudan a validar cadenas bajo ciertas condiciones
 */
+
+const patrones = {
+    nombre: /^[A-Za-zÁÉÍÓÚÑáéíóúñÜü\s]{2,60}$/,
+    boleta: /^\d{10}$/,
+    fecha: /^(0[1-9]|[12]\d|3[01])\/(0[0-9]|1[0-2])\/\d{4}$/
+}
+const mensajes = {
+    nombre: "Solo letras y espacios entre 2 y 60 caracteres.",
+    boleta: "Debe tener exactamente 10 digitos numéricos",
+    fecha: "La fecha debe tener el formato DD/MM/AAAA"
+}
+
+function validarCampo(campo, valor){
+    return patrones[campo].test(valor.trim())
+}
+
+//para validar el formulario debemos ocupar los principios de obtencion y manipulacion de los elementos del DOM
+
+if(typeof document !== 'undefined'){
+    const formulario = document.getElementById('form-registro');
+    formulario.addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        let formularioValido = true;
+        for(const campo of Object.keys(patrones)){
+            const input = document.getElementById(campo);
+            const errorSpan = document.getElementById('error-${campo}');
+            const esValido = validarCampo(campo, input.value)
+            input.classList.toggle('invalido', !esValido)
+            errorSpan.textContent = esValido ? '' : mensajes[campo];
+            if(!esValido) formularioValido = false;
+        }
+        const mensajeExito = document.getElementById('mensaje-exito');
+        mensajeExito.textContent = formularioValido ? 'Registro Exitoso!' : '';
+    })
+}
