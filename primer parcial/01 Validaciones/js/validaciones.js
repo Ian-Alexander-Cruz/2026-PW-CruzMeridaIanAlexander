4f0894d653c9decd938177d6007390ad9db6cfd0
Ian-Alexander-Cruz <icruzm2405@alumno.ipn.mx>
2026-09-14 18:25:49 -0600

    registro completo

---
+++ primer parcial/01 Validaciones/js/validaciones.js
@@ -0,0 +1,11 @@
+/*
+Las validaciones ára ester formulario
+se realizaran mediante el uso de Expresiones
+Regulares, las cuales los vamos a dividir en 3:
+1.- Texto para el nombre
+2.- Numerico para la boleta
+3.- Debe tener un patron para la fecha 
+
+Las expresiones regulares, son patrones que nos 
+ayudan a validar cadenas bajo ciertas condiciones
+*/
